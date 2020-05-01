000241232c76827417158abff049bd1b0290abad
Shin WinMachine <shin.nuriko@gmail.com>
2020-05-01 23:47:15 +0800

    add playerprefs controller for volume and difficulty options

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const string VOLUME_KEY = "volume";
+    const string DIFFICULTY_KEY = "difficulty";
+
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+    const float DEFAULT_VOLUME = 0.5f;
+
+    const float MIN_DIFFICULTY = 0.1f;
+    const float MAX_DIFFICULTY = 2f;
+    const float DEFAULT_DIFFICULTY = 1f;
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Volume is out of range.");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        float prefVolume = PlayerPrefs.GetFloat(VOLUME_KEY);
+        if (prefVolume > 0)
+            return prefVolume;
+        else
+            return DEFAULT_VOLUME;  
+    }
+
+    public static void SetMasterDifficulty(float difficulty)
+    {
+        if (difficulty >= MIN_DIFFICULTY && difficulty <= MAX_DIFFICULTY)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Difficulty is out of range.");
+        }
+    }
+
+    public static float GetMasterDifficulty()
+    {
+        float prefDifficulty = PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+        if (prefDifficulty > 0)
+            return prefDifficulty;
+        else
+            return DEFAULT_DIFFICULTY;
+    }
+}
